e5629e117af1a0ae170474b7374acddc771b5d58
Art <arttay1090@gmail.com>
2019-11-20 18:17:49 -0600

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+const csv = require('csvtojson')
+const csvPath = "./rawCsv/Inventory.csv"
+const { Parser } = require('json2csv');
+
+const fields = ['Product Id', 'Product Sku', 'Product Name', "Product Category", "Sales (Past Week)", "T&T Price", "Lowest Price", "Available", "*On Hold", "*My Price", "*My Cost", "Live Quantity"];
+const opts = { fields };
+
+const write = require('write');
+
+console.log(process.argv[2])
+/*
+csv()
+.fromFile(csvPath)
+.then((jsonObj)=>{
+
+    jsonObj.map((item) => {
+        if (item["Available"] !== item["Live Quantity"]) {
+            if (item["Available"] / item["Live Quantity"] < .5) {
+               item["*My Price"] = parseFloat(item["Lowest Price"]) - .01 < .2 ? .2 : (parseFloat(item["Lowest Price"]) - .01).toFixed(2)   
+            }
+        } else {
+            if (Number(item["*My Price"]) < Number(item["T&T Price"])) {
+                item["*My Price"] = (parseFloat(item["T&T Price"])).toFixed(2) 
+            }
+        }
+
+
+        return item
+    })
+  //  console.log(jsonObj)
+    const parser = new Parser(opts);
+    const csv = parser.parse(jsonObj);
+  //  console.log(csv);
+
+    write.sync('foo.csv', csv); 
+})
+*/
